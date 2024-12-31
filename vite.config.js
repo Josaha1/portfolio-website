1874560285565935388db91467c5f63c3cf3be23
mathas mueangrit <mathascontact@gmail.com>
2024-12-31 21:15:03 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/portfolio-website/",
+  base: '/', // Ensure this matches your deployment URL if nested, otherwise use '/'
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
